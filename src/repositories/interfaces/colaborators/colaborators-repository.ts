160385cd0690c3b774e443aa808356f5362ec445
João Victor Genari <victor.olr@hotmail.com>
2023-10-26 23:16:02 -0300

    refactor: add new types to colaborators repository

--- src/repositories/interfaces/colaborators/colaborators-repository.ts
+++ src/repositories/interfaces/colaborators/colaborators-repository.ts
@@ -1,10 +1,102 @@
+interface NewColaborator {
+  id?: number
+  name: string
+  email: string
+  cpf: string
+  password: string
+  departmentId: number
+  street: string
+  number: string
+  neighborhood: string
+  complement: string
+  city: string
+  state: string
+  cep: string
+}
+
+interface Colaborators {
+  id: number
+  name: string
+  email: string
+  cpf: string
+  active: number
+  avatar: string | null
+  createdAt: string
+  updatedAt: string | null
+  departmentId: number
+  departmentName: string
+  addressId: number
+  addressStreet: string
+  addressNumber: string
+  addressNeighborhood: string
+  addressComplement: string
+  addressCity: string
+  addressState: string
+  addressCep: string
+}
+
+interface ColaboratorById {
+  id: number
+  name: string
+  email: string
+  cpf: string
+  active: number
+  avatar: string | null
+  createdAt: string
+  updatedAt: string | null
+  departmentId: string
+  departmentName: string
+  addressId: number
+  addressStreet: string
+  addressNumber: string
+  addressNeighborhood: string
+  addressComplement: string
+  addressCity: string
+  addressState: string
+  addressCep: string
+}
+
+interface ColaboratorWithEmail {
+  id: number
+  name: string
+  email: string
+  cpf: string
+  active: number
+  avatar: string | null
+  createdAt: string
+  updatedAt: string | null
+  departmentId: string
+  departmentName: string
+  addressId: number
+  addressStreet: string
+  addressNumber: string
+  addressNeighborhood: string
+  addressComplement: string
+  addressCity: string
+  addressState: string
+  addressCep: string
+}
+
+interface ColaboratorByField {
+  id: number
+  name: string
+  email: string
+  password: string
+  cpf: string
+  active: number
+  avatar: string | null
+  departmentId: number
+  createdAt: string
+  updatedAt: string | null
+}
+
 interface Colaborator {
   id?: number
   name?: string
   email?: string
   cpf?: string
   password?: string
-  active?: string
+  active?: number
   avatar?: null
   departmentId?: string
   departmentName?: string
@@ -20,12 +112,12 @@ interface Colaborator {
 }
 
 interface ColaboratorsRepository {
-  findOneBy (field: string, value: unknown): Promise<Colaborator[]>
-  create(payload: Colaborator): Promise<Colaborator[]>
-  findAllColaborators (filters: any, count?: boolean): Promise<Colaborator[]>
-  findColaboratorById (id: number): Promise<Colaborator[]>
-  findColaboratorWithExistingEmail (id: number, email: string): Promise<Colaborator[]>
-  findByIdAndUpdate (id: number, payload: {}): Promise<Colaborator[]>
+  findOneBy (field: string, value: unknown): Promise<ColaboratorByField[]>
+  create(payload: Colaborator): Promise<NewColaborator[]>
+  findAllColaborators (filters: any, count?: boolean): Promise<Colaborators[]>
+  findColaboratorById (id: number): Promise<ColaboratorById[]>
+  findColaboratorWithExistingEmail (id: number, email: string): Promise<ColaboratorWithEmail[]>
+  findByIdAndUpdate (id: number, payload: {}): Promise<ColaboratorById[]>
   disableColaborator (id: number): Promise<Colaborator[]>
 }
 
